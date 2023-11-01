b7f23e4c0c4da0ba42113f1d03c00ed35aaf6720
laoona <laoona@163.com>
2023-11-01 10:55:39 +0800

    refactor: 切换 smarty 引擎

--- index.js
+++ index.js
@@ -10,8 +10,7 @@
 
 const through = require('through2');
 const gutil = require('gulp-util');
-const Smarty = require('./lib/smarty4js');
-const s = new Smarty();
+const Smarty2 = require('smarty.js');
 
 const path = require('path');
 const fs = require('fs');
@@ -19,183 +18,188 @@ const load = require('load-json-file');
 const request = require('then-request');
 
 function getFileName(npath) {
-    if (typeof npath !== 'string') return npath;
-    if (npath.length === 0) return npath;
+  if (typeof npath !== 'string') return npath;
+  if (npath.length === 0) return npath;
 
-    var name = path.basename(npath, path.extname(npath));
-
-    return name;
+  return path.basename(npath, path.extname(npath));
 }
 
 function getDataManifestVal(dataManifest, filePath, baseDir) {
 
-    dataManifest = dataManifest || {};
-    filePath = path.normalize(filePath);
-    baseDir = baseDir || '';
+  dataManifest = dataManifest || {};
+  filePath = path.normalize(filePath);
+  baseDir = baseDir || '';
 
-    let res = '';
-    let key = '';
-    let dMArr = Object.keys(dataManifest);
-    let len = dMArr.length;
-    let n = 0;
+  let res = '';
+  let key = '';
+  let dMArr = Object.keys(dataManifest);
+  let len = dMArr.length;
+  let n = 0;
 
-    for (n; n < len; n++) {
+  for (n; n < len; n++) {
 
-        let k = dMArr[n];
-        let dMval = dataManifest[k];
-        let tmpFilePath = '';
+    let k = dMArr[n];
+    let dMval = dataManifest[k];
+    let tmpFilePath = '';
 
-        tmpFilePath = (/^[\/\\]/gi.test(k)) ? k : path.resolve(baseDir, k);
+    tmpFilePath = (/^[\/\\]/gi.test(k)) ? k : path.resolve(baseDir, k);
 
-        tmpFilePath = path.normalize(tmpFilePath);
+    tmpFilePath = path.normalize(tmpFilePath);
 
-        if (filePath === tmpFilePath) {
-            res = dMval;
-            key = k;
-            break;
-        }
+    if (filePath === tmpFilePath) {
+      res = dMval;
+      key = k;
+      break;
     }
+  }
 
-    return {
-        key: key,
-        value: res
-    };
+  return {
+    key: key,
+    value: res
+  };
 }
 
 function render(options) {
-    options = options || {};
+  options = options || {};
+  const baseDir = options.baseDir;
+  const leftDelimiter = options.left_delimiter || '{{';
+  const rightDelimiter = options.left_delimiter || '}}';
 
-    s.conf.left_delimiter = options.left_delimiter || '{{';
-    s.conf.right_delimiter = options.right_delimiter || '}}';
-    s.conf.rootDir = options.rootDir;
+  Smarty2.prototype.left_delimiter = leftDelimiter;
+  Smarty2.prototype.right_delimiter = rightDelimiter;
 
-    let baseDir = options.baseDir;
-    baseDir && s.setBasedir(baseDir);
+  Smarty2.prototype.getTemplate = function (name) {
+    return fs.readFileSync(`${baseDir}/${name}`, 'utf8');
+  }
 
-    let templateDataDir = options.templateDataDir;
-    let dataManifest = options.dataManifest || {};
-    let rootDir = options.rootDir || '';
-    let constPath = options.constPath || '';
-    let commonConst = null;
-    let commonConstTemp = {};
-    let commonConstRoot = {};
+  Smarty2.prototype.getFile = function(name) {
+    return fs.readFileSync(`${baseDir}/${name}`, 'utf8');
+  }
 
-    let constPathTemp = path.resolve(templateDataDir, constPath);
-    let constPathRoot = path.resolve(rootDir, constPath);
+  let templateDataDir = options.templateDataDir;
+  let dataManifest = options.dataManifest || {};
+  let rootDir = options.rootDir || '';
+  let constPath = options.constPath || '';
+  let commonConst = null;
+  let commonConstTemp = {};
+  let commonConstRoot = {};
 
-    try {
-        if (options.constPath) {
-            if (fs.existsSync(constPathTemp)) {
-                commonConstTemp = load.sync(constPathTemp);
-            }
+  let constPathTemp = path.resolve(templateDataDir, constPath);
+  let constPathRoot = path.resolve(rootDir, constPath);
 
-            if (fs.existsSync(constPathRoot)) {
-                commonConstRoot = load.sync(constPathRoot);
-            }
-        }
-    } catch (e) {
-      return through.obj(function (file, enc, cb) {
-        this.emit('error', new gutil.PluginError('gulp-smarty4js-render', e));
-        return cb();
-      });
+  try {
+    if (options.constPath) {
+      if (fs.existsSync(constPathTemp)) {
+        commonConstTemp = load.sync(constPathTemp);
+      }
+
+      if (fs.existsSync(constPathRoot)) {
+        commonConstRoot = load.sync(constPathRoot);
+      }
+    }
+  } catch (e) {
+    return through.obj(function (file, enc, cb) {
+      this.emit('error', new gutil.PluginError('gulp-smarty4js-render', e));
+      return cb();
+    });
+  }
+
+  commonConst = Object.assign(commonConstRoot, commonConstTemp);
+
+  return through.obj(function (file, enc, cb) {
+
+    const self = this;
+    if (file.isNull()) {
+      this.push(file);
+      return cb();
     }
 
-    commonConst = Object.assign(commonConstRoot, commonConstTemp);
+    if (file.isStream()) {
+      this.emit('error', new gutil.PluginError('gulp-smarty4js-render', 'Streaming not supported'));
+    }
 
-    return through.obj(function (file, enc, cb) {
+    let dataFile = '';
+    let data = {};
 
-        var self = this;
-        if (file.isNull()) {
-            this.push(file);
+    let _me = this;
+
+    const compileAsync = function (data, file, enc, cb) {
+      Object.assign(data, commonConst);
+
+      let html = '';
+      let jSmart = null;
+
+      const fileContents = file.contents.toString('utf8');
+
+      try {
+        jSmart = new Smarty2(fileContents);
+        html = jSmart.fetch(data);
+      } catch (e) {
+        html = '';
+        this.emit('error', new gutil.PluginError('gulp-smarty4js-render', e));
+        return cb();
+      }
+
+      if (file.isBuffer()) {
+        file.contents = new Buffer(html);
+        file.path = gutil.replaceExtension(file.path, '.html');
+      }
+
+      this.push(file);
+      jSmart = null;
+      return cb();
+    };
+
+    let dM = getDataManifestVal(dataManifest, file.path, templateDataDir);
+    let dMVal = dM.value;
+    let dMKey = dM.key.replace(/[\/\\]/gi, '$');
+    const valDataFile = path.resolve(templateDataDir, dMVal);
+    dMKey = dMKey.replace(/^\.+/gi, '');
+
+    if (templateDataDir) {
+      dataFile = path.normalize([path.resolve(templateDataDir + '/data/', getFileName(dMKey)), '.json'].join(''));
+    }
+
+    if (fs.existsSync(valDataFile) && !fs.lstatSync(valDataFile).isDirectory()) {
+      try {
+        data = load.sync(valDataFile);
+      } catch (e) {
+        this.emit('error', new gutil.PluginError('gulp-smarty4js-render', e));
+        return cb();
+      }
+
+      compileAsync.apply(_me, [data, file, enc, cb]);
+    } else if (fs.existsSync(dataFile)) {
+      try {
+        data = load.sync(dataFile);
+      } catch (e) {
+        this.emit('error', new gutil.PluginError('gulp-smarty4js-render', e));
+        return cb();
+      }
+
+      compileAsync.apply(_me, [data, file, enc, cb]);
+    } else {
+
+      if (/^http(s|):\/\//gi.test(dMVal)) {
+
+        request('GET', dMVal).done(function (res) {
+
+          let data = (res.getBody().toString());
+
+          try {
+            data = JSON.parse(data);
+          } catch (e) {
+            self.emit('error', new gutil.PluginError('gulp-smarty4js-render', e));
             return cb();
-        }
+          }
 
-        if (file.isStream()) {
-            this.emit('error', new gutil.PluginError('gulp-smarty4js-render', 'Streaming not supported'));
-        }
-
-        var dataFile = '';
-        var data = {};
-
-        var _me = this;
-
-        var compileAsync = function (data, file, enc, cb) {
-            var compiler = null;
-            var html = '';
-
-            Object.assign(data, commonConst);
-
-            try {
-                compiler = s.compile(file.path);
-                html = compiler.render(data);
-            } catch (e) {
-                html = '';
-                compiler = null;
-
-                this.emit('error', new gutil.PluginError('gulp-smarty4js-render', e));
-                return cb();
-            }
-
-            if (file.isBuffer()) {
-                file.contents = new Buffer(html);
-                file.path = gutil.replaceExtension(file.path, '.html');
-            }
-
-            this.push(file);
-            return cb();
-        };
-
-        let dM = getDataManifestVal(dataManifest, file.path, templateDataDir);
-        let dMVal = dM.value;
-        let dMKey = dM.key.replace(/[\/\\]/gi, '$');
-        var valDataFile = path.resolve(templateDataDir, dMVal);
-        dMKey = dMKey.replace(/^\.+/gi, '');
-
-        if (templateDataDir) {
-            dataFile = path.normalize([path.resolve(templateDataDir + '/data/', getFileName(dMKey)), '.json'].join(''));
-        }
-
-        if (fs.existsSync(valDataFile) && !fs.lstatSync(valDataFile).isDirectory()) {
-            try {
-                data = load.sync(valDataFile);
-            } catch (e) {
-                this.emit('error', new gutil.PluginError('gulp-smarty4js-render', e));
-                return cb();
-            }
-
-            compileAsync.apply(_me, [data, file, enc, cb]);
-        } else if (fs.existsSync(dataFile)) {
-
-            try {
-                data = load.sync(dataFile);
-            } catch (e) {
-                this.emit('error', new gutil.PluginError('gulp-smarty4js-render', e));
-                return cb();
-            }
-
-            compileAsync.apply(_me, [data, file, enc, cb]);
-        } else {
-            if (/^http(s|):\/\//gi.test(dMVal)) {
-
-                request('GET', dMVal).done(function (res) {
-
-                    var data = (res.getBody().toString());
-
-                    try {
-                        data = JSON.parse(data);
-                    } catch (e) {
-                        self.emit('error', new gutil.PluginError('gulp-smarty4js-render', e));
-                        return cb();
-                    }
-
-                    compileAsync.apply(_me, [data, file, enc, cb]);
-                });
-            } else {
-
-                compileAsync.apply(_me, [data, file, enc, cb]);
-            }
-        }
-    });
+          compileAsync.apply(_me, [data, file, enc, cb]);
+        });
+      } else {
+        compileAsync.apply(_me, [data, file, enc, cb]);
+      }
+    }
+  });
 }
 
 module.exports = render;
